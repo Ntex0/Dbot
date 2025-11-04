f2b3ebfc392294db79d1f1e36118144b8144552f
Uchenna King <uchenna.king3@mail.dcu.ie>
2025-11-04 00:19:41 +0000

    Added GetPlayerHead helper function to display command

--- commands/display.py
+++ commands/display.py
@@ -1,15 +1,27 @@
 import discord
 from discord.ext import commands
 import requests
+import json 
 from discord import app_commands
+import os
+from main import ROOT
 
-def PlayerUUID(player_name):
+
+def PlayerUUID(player_name, cache):
     uuid_api_url = f"https://api.mojang.com/users/profiles/minecraft/{player_name}" # accesses minecraft api to get uid to pass to other api for images
     response = requests.get(uuid_api_url)
     if response.status_code == 200:
         data_to_parse = response.json() # json file to be parsed for uuid
         player__uuid = data_to_parse.get("id")
         print(player__uuid)
+
+def GetPlayerHead(player_uuid):
+
+    head_url = f"https://crafatar.com/avatars/{player.uuid}"
+    response = requests.get(head_url)
+    if response.status_code == 200:
+        pass 
+
 
 class Display(commands.Cog):
 
@@ -24,8 +36,17 @@ class Display(commands.Cog):
         await interaction.response.send_message(list_of_players, ephemeral=True)
         for player_name in parsed_list_players:
             player_name = player_name.strip()
-            PlayerUUID(player_name)
+            parsed_uuid = PlayerUUID(player_name) # Helper function which grabs our players UUID
+            GetPlayerHead(parsed_uuid) # Helper function that then gets our players head image link 
             await interaction.followup.send(f"Player is {player_name}")
+
+cache_file =  os.path.join(ROOT, "cache", "player_cache.json")
+
+if not os.path.exists(cache_file):
+    with open(cache_file, "w") as cache:
+        cache.write("{}")
+
+cache = json.load(cache_file)
 
 async def setup(bot):
     await bot.add_cog(Display(bot))
